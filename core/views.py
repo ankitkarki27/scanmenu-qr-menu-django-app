2f29805be38bcd9b167cac74c663f71c068c0306
ankitkarki27 <ankitkarki8088@gmail.com>
2025-04-23 19:31:22 +0545

    project push: QR Menu Generator

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from .forms import QRCodeForm
 import qrcode
 import os
 from django.conf import settings
-
+from django.http import HttpResponse
 # views.py
 def generate_qr_code(request):
     if request.method == 'POST':
@@ -14,15 +14,24 @@ def generate_qr_code(request):
             
             # generate the QR code
             qr = qrcode.make(url)
-            print(qr)
-        
-            #save
-            qr.save('test_qr.png')            
+            file_name= restaurant_name.replace("","_").lower() + "_menu.png"
+            file_path=os.path.join(settings.MEDIA.ROOT,file_name) #media/restaurant_name_menu.png
+            qr.save(file_path)
+            # qr.save('file_name')    
+            
+            context={
+                'form': form,
+                'qr_code': file_name,
+                'restaurant_name': restaurant_name,
+                'url': url,
+            }
+            return render(request,'your_qr.html',context)
+
     else:
         form=QRCodeForm()
         context={
             'form': form,
+            
         }
-    
         return render(request, 'qr_code.html', context)
 

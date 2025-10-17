3f3ecd47bde6f6694ab6002e3f76a605da582354
ankitkarki27 <ankitkarki8088@gmail.com>
2025-10-17 22:58:39 +0545

    Add health check endpoint for Render

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
+def healthz(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('healthz', healthz),
     
     path('', views.generate_qr_code, name='qr_code'),
  
